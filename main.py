5bbaf6eeb7cee30b43bc2fec07682c1650d0a466
Archit Agarwal <81604364+Potatoes98@users.noreply.github.com>
2022-02-24 21:48:27 +0000

    Menu completed, running of files works, exit works.

--- main.py
+++ main.py
@@ -4,14 +4,27 @@ import math
 import random
 import sys
 from modules.util import *
+import readkeys
 
 files = [f for f in os.listdir("people") if os.path.isfile(os.path.join("people", f))]
 
-mainText = files
-extraText = []
-mainText.pop(0)
+mainText = [x.split(".")[0].capitalize() for x in files if x != "__init__.py"]
+extraText = ["Main Test File || 0.3.1", "Made by Archit", "Files are shown below", ""]
 
 mainText.append("Exit")
 
 while True:
-  createMenu
+  selection = createMenu(mainText, 0, 63, extraText, 63)
+
+  if selection == "Exit":
+    clear()
+    print("Test File Exited")
+    break
+  else:
+    clear()
+    f = open("people/" + selection.lower() + ".py")
+    exec(f.read())
+    f.close()
+    print("\n\nPROGRAM COMPLETE")
+    print("Press any key to continue")
+    readkeys.getkey()
---
+++ people/archit.py
@@ -0,0 +1,9 @@
+import os
+import time
+import math
+import random
+import sys
+from modules.util import *
+import readkeys
+
+typeReplace("yo, yo, wassup, bro?")
