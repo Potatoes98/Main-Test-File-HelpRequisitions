77d0179e727359d7ab2861f406d9e8e52e19cd80
Archit Agarwal <81604364+Potatoes98@users.noreply.github.com>
2022-02-24 23:00:50 +0000

    Reset version after breakage. Added a sort.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import readkeys
 files = [f for f in os.listdir("people") if os.path.isfile(os.path.join("people", f))]
 
 mainText = [x.split(".")[0].capitalize() for x in files if x != "__init__.py"]
+mainText.sort()
 extraText = ["Main Test File || 0.3.1", "Made by Archit", "Files are shown below", ""]
 
 mainText.append("Exit")
