a87b83b8124b8798642bb8fae20f18115c121085
Archit Agarwal <81604364+Potatoes98@users.noreply.github.com>
2022-02-24 21:35:32 +0000

    File List completed

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ from modules.util import *
 
 files = [f for f in os.listdir("people") if os.path.isfile(os.path.join("people", f))]
 
-mainText = []
+mainText = files
 extraText = []
+mainText.pop(0)
+
+mainText.append("Exit")
+
+while True:
+  createMenu
